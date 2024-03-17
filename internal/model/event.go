3632825620e794322f6453eddae0c927d5881ad7
gildemberg-santos <gildemberg.santos@gmail.com>
2024-03-16 21:31:51 -0300

    feat: :sparkles: Criar model para cadastrar eventos

--- internal/model/event.go
+++ internal/model/event.go
@@ -1,7 +1,36 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
 
-type Event struct {
+	"gorm.io/gorm"
+)
+
+type Events struct {
 	gorm.Model
+	CompanyID       int32     `json:"company_id"`
+	ScriptID        int32     `json:"script_id"`
+	ScriptVersionID int32     `json:"script_version_id"`
+	ClientVersion   string    `json:"client_version"`
+	UserID          string    `json:"user_id"`
+	SessionID       string    `json:"session_id"`
+	Event           string    `json:"event"`
+	Time            time.Time `json:"time"`
+	Mobile          string    `json:"mobile"`
+	Referrer        string    `json:"referrer"`
+	Location        string    `json:"location"`
+	ScriptName      string    `json:"script_name"`
+	AnalyticChannel string    `json:"analytic_channel"`
+	Score           int32     `json:"score"`
+}
+
+func NewEvents() *Events {
+	return &Events{}
+}
+
+func (e *Events) Save() error {
+	conection := ConectionDB{}
+	conection.Open()
+	defer conection.Close()
+	return conection.DB.Create(e).Error
 }
